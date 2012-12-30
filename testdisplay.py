fa9d4f83698bb433535c3ad7372427d673101819
unknown <jruokonen@ovi.com>
2012-12-30 14:25:18 +0200

    Refactored bars into more reusable class.

--- testdisplay.py
+++ testdisplay.py
@@ -3,6 +3,7 @@ Test for game statistics display module
 '''
 import os, sys
 import pygame
+import math
 from pygame.locals import *
 from resource import *
 from display import *
@@ -16,7 +17,6 @@ class TestDisplay:
 
         self.olutta = 100
         self.kaljaa = 50
-
         while 1:
             screen.fill((0,0,0))
             self.window.reset()
@@ -41,9 +41,17 @@ class TestDisplay:
         if (self.kaljaa < 0):
             self.kaljaa = 160
 
-        self.window.addBar("LaTrappea", 200, 400)
-        self.window.addBar("Olutta", self.olutta, 200, (255, 64, 0))
-        self.window.addBar("Kaljaa", self.kaljaa, 160, (32, 255, 32)) 
+        group2 = BarGroup(int(300), int(300))
+        group2.addBar("Kinkkua", 150, 1000, (128,192,64))
+        group2.addBar("Viljaa", self.olutta, 2000, (192,225,50))
+        group2.addBar("Toolsseja", self.kaljaa, 200, (64,32,192))
+        self.window.addBarGroup(group2)
+
+        group = BarGroup(int(200), int(100))
+        group.addBar("LaTrappea", 200, 400)
+        group.addBar("Olutta", self.olutta, 200, (255,64,0))
+        group.addBar("Kaljaa", self.kaljaa, 160, (32,255,32))
+        self.window.addBarGroup(group)
 
     def testText(self):
         self.window.addText("La Trappen markkinahinta: 12")
